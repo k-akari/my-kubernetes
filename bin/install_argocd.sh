d41d2024b0af8d6990d2d94050f7d60b8b8d52b8
k-akari <tennis.akari.abcdefg@gmail.com>
2022-07-28 09:46:57 +0900

    ループの中でエラーメッセージが表示されていたのを解消した。

--- bin/install_argocd.sh
+++ bin/install_argocd.sh
@@ -8,13 +8,17 @@
 aws eks update-kubeconfig --region ap-northeast-1 --name eks-cluster --profile akari_mfa
 echo -e "\n"
 
+# Install ESO from Helm chart repository
+helm repo add external-secrets https://charts.external-secrets.io
+helm install external-secrets external-secrets/external-secrets -n external-secrets --create-namespace --set installCRDs=true
+
 # Install ArgoCD
 ns_argocd=`kubectl get ns -o json | jq -r '.items[] | .metadata.name' | grep argocd`
 if [ -z "$ns_argocd" ]; then
   kubectl create namespace argocd
-  echo -e "Successfully created namespace of argocd."
+  echo -e "Successfully created namespace of argocd.\n"
 else
-  echo -e "Namespace of argocd already exists."
+  echo -e "Namespace of argocd already exists.\n"
 fi
 echo -e "\n"
 
@@ -35,42 +39,38 @@ else
   echo -e "ArgoCD manifest has already been applied.\n"
 fi
 
-# Show External IP, Initial User, and Initial Password
-exists_argocd_server=`kubectl get svc argocd-server -n argocd -o json > /dev/null 2>&1`
-if [ -n "$exists_argocd_server" ]; then
-  while [ -n "$exists_argocd_server" ]; do
+# Get External IP
+error_argocd_server=`kubectl get svc argocd-server -n argocd -o json 2>&1 > /dev/null`
+if [ -n "$error_argocd_server" ]; then
+  echo -e "\nWaiting to start argocd-server"
+  while [ -n "$error_argocd_server" ]; do
     echo -n "."
     sleep 1
-    exists_argocd_server=`kubectl get svc argocd-server -n argocd -o json > /dev/null 2>&1`
+    error_argocd_server=`kubectl get svc argocd-server -n argocd -o json 2>&1 > /dev/null`
   done
 fi
 external_ip=`kubectl get svc argocd-server -n argocd -o json | jq -r '.status.loadBalancer.ingress[].hostname'`
 echo -e "\n"
-initial_user="admin"
-initial_password=`kubectl -n argocd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo`
-while [ -z "$initial_password" ]; do
-  echo -n "."
-  sleep 1
-  initial_password=`kubectl -n argocd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo`
-done
-echo "External IP     : $external_ip"
-echo "Initial User    : $initial_user"
-echo "Initial Password: $initial_password"
-echo -e "\n"
 
-# Wait for ArgoCD to start
-accessable_to_argocd=`nslookup -type=ns $external_ip | grep "Authoritative answers can be found from"`
-while [ -z "$accessable_to_argocd" ]; do
-  echo -n "."
-  sleep 1
-  accessable_to_argocd=`nslookup -type=ns $external_ip | grep "Authoritative answers can be found from"`
-done
+# Get Initial User
+initial_user="admin"
+
+# Get Initial Password
+error_argocd_initial_admin_secret=`kubectl -n argocd get secret/argocd-initial-admin-secret 2>&1 > /dev/null`
+if [ -z "$error_argocd_initial_admin_secret" ]; then
+  initial_password=`kubectl -n argocd get secret/argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo`
+fi
 
 # Login to ArgoCD
-argocd login "$external_ip" --username admin --password "$initial_password" --insecure
+if [ -n "$initial_password" ]; then
+  accessable_to_argocd=`nslookup -type=ns $external_ip | grep "Authoritative answers can be found from"`
+  while [ -z "$accessable_to_argocd" ]; do
+    echo -n "."
+    sleep 1
+    accessable_to_argocd=`nslookup -type=ns $external_ip | grep "Authoritative answers can be found from"`
+  done
+  argocd login "$external_ip" --username admin --password "$initial_password" --insecure
 
-initial_secret=`kubectl get secret/argocd-initial-admin-secret -n argocd -o json | jq -r '.metadata.name'`
-if [ -n "$initial_secret" ]; then
   # Update password for admin
   new_password=`openssl rand -base64 6`
   echo "New Password : $new_password"
@@ -78,9 +78,15 @@ if [ -n "$initial_secret" ]; then
 
   # Remove the secret resource containing initial password
   kubectl --namespace argocd delete secret/argocd-initial-admin-secret
-else
-  echo -n "Secret which stored the initial password for ArgoCD does not exist.\n"
 fi
 
-# Create application
+echo "***********************************"
+echo "External IP      : $external_ip"
+echo "Initial User     : $initial_user"
+echo "Initial Password : $initial_password"
+echo "New Password     : $new_password"
+echo "***********************************"
+echo -e "\n"
+
+# Deploy application
 kubectl apply -f ./application.yaml
